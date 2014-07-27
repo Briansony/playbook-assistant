67515f3d7ac9b859445e3d008f884957a0a65d61
ochirkov <ironloriin20@gmail.com>
2014-07-28 00:14:46 +0300

    worker fixes

--- helpers/params.py
+++ helpers/params.py
@@ -4,22 +4,18 @@ parser = argparse.ArgumentParser()
 
 subparsers = parser.add_subparsers(help='commands')
 
-
-# TODO: if mode is not specified default is default
 # TODO: fix --roles=bla,foo
 
 # A create command
 create_parser = subparsers.add_parser('create', help='Create playbook')
 create_parser.add_argument('-t', '--task', choices=('create', 'add'), required=True, help='Task name')
-create_parser.add_argument('-m', '--mode', choices=('full', 'default'), help='Mode of playbook')
+create_parser.add_argument('-m', '--mode', choices=('full', 'default'), default='default', help='Mode of playbook')
 create_parser.add_argument('-r', '--roles', nargs='+', required=True, help='List of roles')
 create_parser.add_argument('-n', '--name', action='store', required=True, help='Name of playbook')
 create_parser.add_argument('-p', '--path', action='store', required=True, help='Path where playbooks are stored')
 create_parser.add_argument('-w', '--without_inventory', action='store_true', default=False, help='Whether use inventory')
 
-add_parser = subparsers.add_parser('add', help='Add role to playbook')
-add_parser.add_argument('-t', '--task', choices=('create', 'add'), required=True, help='Task name')
-add_parser.add_argument('-n', '--name', action='store', required=True, help='Name of playbook')
-add_parser.add_argument('-r', '--roles', nargs='+', required=True, help='List of roles')
+# download_parser = subparsers.add_parser('download', help='Download playbook from CVS')
+
 
 args = vars(parser.parse_args())
